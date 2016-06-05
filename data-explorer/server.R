83fb92b9f2672b7e4e0f9f6c51c5cb5db1346c67
alexander-poon <alexander.poon@vanderbilt.edu>
2016-06-05 13:04:26 -0500

    add update select input

--- data-explorer/server.R
+++ data-explorer/server.R
@@ -1,17 +1,19 @@
 ## Data Explorer
 # server.R
 
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
 
     # Adjust color, opacity of highlighted district
     df_highlight <- reactive({
-        if (input$district != "") {
-            df[df$system_name == input$district, ]$state <- 2
-            df[df$system_name == input$district, ]$opac <- 1
+
+        if (input$highlight_dist != "") {
+            df[df$system_name == input$highlight_dist, ]$state <- 2
+            df[df$system_name == input$highlight_dist, ]$opac <- 1
         }
 
         # Filter for missing data based on input
         df[!is.na(df[names(df) == input$outcome]), ]
+
     })
 
     # Create tooltip with district name, selected x and y variables
@@ -26,9 +28,12 @@ shinyServer(function(input, output) {
                     row[names(row) == input$outcome], "%")
     }
 
-    # Extract district number of hovered point for secondary graphs, set default to State (district = 0)
+    # Extract district number of clicked point for secondary graphs, set default to State (district = 0)
+    # Update highlighted district on point click
     clicked <- reactiveValues(district = "State of Tennessee")
+
     click_district <- function(data, ...) {
+        updateSelectInput(session, "highlight_dist", selected = as.character(data$system_name))
         clicked$district <- as.character(data$system_name)
         
         str(clicked$district)
@@ -58,33 +63,35 @@ shinyServer(function(input, output) {
             hide_legend("fill") %>%
             set_options(width = 'auto', height = 725) %>%
             handle_click(click_district)
+
     })
 
     plot %>% bind_shiny("plot")
-    
+
     # Secondary Plot 1 - Horizontal bar chart with demographics 
     plot2 <- reactive({
-        
+
         district_data <- df_highlight() %>%
             filter(system_name == clicked$district) %>%
             select(one_of(c("system_name", "Pct_BHN", "Pct_ED", "Pct_EL", "Pct_SWD"))) %>%
             rename("% BHN" = Pct_BHN, "% ED" = Pct_ED, "% EL" = Pct_EL, "% SWD" = Pct_SWD) %>%
             gather("demographic", "Percentage", 2:5)
-        
+
         district_data %>%
-            ggvis(~Percentage, ~factor(demographic)) %>%
-            layer_rects(x2 = 0, height = band(), fill := "blue", fillOpacity := 0.3, fillOpacity.hover := 0.8) %>%
+            ggvis(~Percentage, ~demographic) %>%
+            layer_rects(x2 = 0, height = band(), 
+                        fill := "blue", fillOpacity := 0.3, fillOpacity.hover := 0.8) %>%
             add_axis("x", grid = FALSE) %>%
             add_axis("y", title = "", grid = FALSE) %>%
             scale_numeric("x", domain = c(0, 100)) %>%
             set_options(width = 'auto', height = 350)
-        
+    
     })
-    
+
     plot2 %>% bind_shiny("plot3")
-    
+
     output$text1 <- renderText({paste(clicked$district, "Demographics", sep = " ")})
-    
+
     # Secondary plot 2 - Bar chart of proficiency for selected district
     plot3 <- reactive({
 
@@ -108,10 +115,10 @@ shinyServer(function(input, output) {
             set_options(width = 'auto', height = 350)
 
     })
-    
+
     plot3 %>% bind_shiny("plot2")
-    
+
     output$text2 <- renderText({paste(clicked$district, "Proficiency in All Subjects", sep = " ")})
-    
+
     }
 )
--- data-explorer/ui.R
+++ data-explorer/ui.R
@@ -18,6 +18,7 @@ shinyUI(navbarPage("Data Explorer", position = "fixed-top",
                        br(),
                        h4(textOutput("text1")),
                        ggvisOutput("plot3"),
+                       hr(),
                        h4(textOutput("text2")),
                        ggvisOutput("plot2")
                        
@@ -27,7 +28,7 @@ shinyUI(navbarPage("Data Explorer", position = "fixed-top",
                 column(3, offset = 1,
                     selectInput("char", label = "Select a District Characteristic:", 
                                 choices = district_char, selected = "Pct_ED", width = 500),
-                    selectInput("district", label = "Optional: Highlight a District", 
+                    selectInput("highlight_dist", label = "Optional: Highlight a District", 
                                 choices = c("", district_list), selected = NULL, width = 500)
                 ),
                 column(3,
