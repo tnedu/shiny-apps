7db837cf54bfb1e1821c93dceb272af66bda2fbb
alexander-poon <alexander.poon@vanderbilt.edu>
2016-07-15 14:37:26 -0500

    initial commit

---
+++ explorer-motion/server.R
@@ -0,0 +1,14 @@
+## Data Explorer - Motion Chart
+# server.R
+
+shinyServer(function(input, output) {
+
+    output$chart <- renderGvis({
+        gvisMotionChart(ach_profile, idvar = "District", timevar = "Year",
+                        xvar = "Percent Economically Disadvantaged", yvar = "Math Percent Proficient or Advanced",
+                        sizevar = "Student Enrollment", colorvar = "CORE Region",
+                        options = list(width = 1000, height = 800))
+    })
+
+    }
+)
