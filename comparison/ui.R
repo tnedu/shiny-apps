0b1f4d523da96761d55b11bb99572717da5429af
alexander-poon <alexander.poon@vanderbilt.edu>
2017-05-09 15:16:51 -0500

    add help text to select tool

--- comparison/ui.R
+++ comparison/ui.R
@@ -38,34 +38,39 @@ navbarPage("Comparison Tool", theme = "doe-style.css",
                         choices = c("2015-16" = 2016, "2014-15" = 2015), selected = 2015)
                 ))
             ),
-            # Message shown on initializing
-            conditionalPanel("input.button == 0",
-                column(7,
+            column(7,
+                # Message shown on initializing
+                conditionalPanel("input.button == 0",
                     h4("Using the input widgets on the left, select a school district and one or
                         more district characteristics."),
                     br(),
                     p("For the selected district, this tool will identify the most similar districts
                         based on the selected characteristics and display data for a selected outcome.")
+                ),
+                # Message shown if no characteristics are selected
+                conditionalPanel("input.button >= 1",
+                    hidden(tags$div(id = "request_input",
+                        h4("Please select a district and one or more characteristics.")
+                    )),
+                    # Output
+                    tags$div(id = "output",
+                             h4(textOutput("header_plot")),
+                             br(),
+                             rbokehOutput("plot", height = "600px"),
+                             br(),
+                             h4(textOutput("header_table")),
+                             br(),
+                             tableOutput("table")
+                    )
                 )
             ),
-            conditionalPanel("input.button >= 1",
-                # Message shown if no characteristics are selected
-                hidden(tags$div(id = "request_input",
-                    fluidRow(
-                        h4("Please select a district and one or more district characteristics.")
-                    )
-                )),
-                # Output panel
-                tags$div(id = "output",
-                    column(7,
-                        h4(textOutput("header_plot")),
-                        br(),
-                        rbokehOutput("plot", height = "600px"),
-                        br(),
-                        h4(textOutput("header_table")),
-                        br(),
-                        tableOutput("table")
-                    )
+            fluidRow(
+                column(10, offset = 1,
+                       hr(),
+                       p("Created in", tags$a(href = "http://shiny.rstudio.com/", "Shiny"),
+                         "for the Tennessee Department of Education."),
+                       br(),
+                       br()
                 )
             )
         )
@@ -73,6 +78,7 @@ navbarPage("Comparison Tool", theme = "doe-style.css",
 
     tabPanel("Select", icon = icon("hand-pointer-o"),
         fluidRow(
+            # Sidebar with input widgets
             column(3, offset = 1,
                 wellPanel(
                     h4("Select Districts"),
@@ -82,17 +88,36 @@ navbarPage("Comparison Tool", theme = "doe-style.css",
                     br(),
                     uiOutput("comparison_districts")
                 ),
-                wellPanel(
+                hidden(wellPanel(id = "foo",
                     selectInput(inputId = "outcome2", label = "Select an outcome:",
                         choices = outcome_list, selected = "Math", width = 400),
-                    br(),
                     selectInput(inputId = "year2", label = "School Year:",
                         choices = c("2015-16" = 2016, "2014-15" = 2015), selected = 2015)
-                )
+                ))
             ),
+            # Output
             column(7,
-                rbokehOutput("plot2", height = "600px")
+                # Message shown on initializing
+                conditionalPanel("input.district2 == ''",
+                    h4("Using the input widgets on the left, select a school district and one or
+                        more comparison districts."),
+                    br(),
+                    p("For the selected districts, this tool will display data for a selected outcome.")
+                ),
+                rbokehOutput("plot2", height = "600px"),
+                br(),
+                tableOutput("table2")
+            )
+        ),
+        fluidRow(
+            column(10, offset = 1,
+                hr(),
+                p("Created in", tags$a(href = "http://shiny.rstudio.com/", "Shiny"),
+                    "for the Tennessee Department of Education."),
+                br(),
+                br()
             )
         )
     )
+
 )
